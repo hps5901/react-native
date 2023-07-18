3ab4e305b9e95a97d6f0a6fc30927c6d02b4af29
hps5901 <hardikjanuary2022@gmail.com>
2023-07-18 15:03:46 +0530

    added starter templates (assets/comp/constants/styles)

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native';
+
+export default function Page() {
+    return (
+<>
+            <Text>Welcome To the Home page</Text>
+            <Text>Welcome To the Home page</Text>
+            <Text>Welcome To the Home page</Text>
+            <Text>Welcome To the Home page</Text>
+            <Text>Welcome To the Home page</Text>
+            <Text>Welcome To the Home page</Text>
+        
+</>
+    );
+}
